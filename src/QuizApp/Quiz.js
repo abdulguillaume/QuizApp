2c617f99945bc45d88d45af3c140b74850d8032e
Abdul Guillaume <abdulguillaume@gmail.com>
2017-03-24 19:25:24 -0700

    starting html UI

--- src/QuizApp/Quiz.js
+++ src/QuizApp/Quiz.js
@@ -1,14 +1,14 @@
-var Helper;
-(function (Helper) {
+var helper;
+(function (helper) {
     var idAnswer = 0, idQuestion = 0;
     function getIdAnswer() {
         return ++idAnswer;
     }
-    Helper.getIdAnswer = getIdAnswer;
+    helper.getIdAnswer = getIdAnswer;
     function getIdQuestion() {
         return ++idQuestion;
     }
-    Helper.getIdQuestion = getIdQuestion;
+    helper.getIdQuestion = getIdQuestion;
     function getRandomPosInArray(current_pos, maxval) {
         var pos;
         while (true) {
@@ -19,8 +19,12 @@ var Helper;
             }
         }
     }
-    Helper.getRandomPosInArray = getRandomPosInArray;
-})(Helper || (Helper = {}));
+    helper.getRandomPosInArray = getRandomPosInArray;
+    function htmlTagReplace(data) {
+        return data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    }
+    helper.htmlTagReplace = htmlTagReplace;
+})(helper || (helper = {}));
 var Quiz = (function () {
     function Quiz() {
         this.questions = [];
@@ -55,6 +59,15 @@ var Quiz = (function () {
         }
         return score;
     };
+    //maximum score for the quiz
+    Quiz.prototype.maxScore = function () {
+        var score = 0;
+        for (var _i = 0, _a = this.questions; _i < _a.length; _i++) {
+            var question = _a[_i];
+            score += question.maxScore;
+        }
+        return score;
+    };
     Quiz.prototype.answerQuestion = function (question_id, answer_id) {
         var q = this.questions.filter(function (x) { return x.id === question_id; });
         if (q.length == 0)
@@ -68,7 +81,8 @@ var Answer = (function () {
     function Answer(_definition, _score) {
         this._definition = _definition;
         this._score = _score;
-        this._id = Helper.getIdAnswer();
+        this._id = helper.getIdAnswer();
+        this._definition = helper.htmlTagReplace(this._definition);
     }
     Object.defineProperty(Answer.prototype, "id", {
         get: function () {
@@ -79,7 +93,7 @@ var Answer = (function () {
     });
     Object.defineProperty(Answer.prototype, "definition", {
         get: function () {
-            return this.id + ' - ' + this._definition;
+            return this._definition;
         },
         enumerable: true,
         configurable: true
@@ -111,11 +125,18 @@ var Question = (function () {
         this.sentence = sentence;
         this.answer = answer;
         this.canRetake = canRetake;
+        this.sentence = helper.htmlTagReplace(this.sentence);
         this.answer_pool = [];
         this.answer_pool.push(answer);
         this.final_score = -1;
-        this._id = Helper.getIdQuestion();
+        this._id = helper.getIdQuestion();
     }
+    Question.prototype.getAllAnswers = function () {
+        return this.answer_pool;
+    };
+    Question.prototype.getSentence = function () {
+        return this.sentence;
+    };
     Object.defineProperty(Question.prototype, "id", {
         get: function () {
             return this._id;
@@ -126,6 +147,13 @@ var Question = (function () {
     Object.defineProperty(Question.prototype, "score", {
         get: function () {
             return this.final_score == -1 ? 0 : this.final_score;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Question.prototype, "maxScore", {
+        get: function () {
+            return this.answer.score;
         },
         enumerable: true,
         configurable: true
@@ -164,8 +192,9 @@ var Question = (function () {
         }
     };
     Question.prototype.isValidForQuiz = function () {
-        //at least 2 answers in order to be eligible for quiz => question is valid
-        return (this.answer_pool.length > 1);
+        //1 of the requirements - Give the user a minimum of 3 and no more than 5 choices
+        //between 3 and 5 answers in order to be valid question for the quiz
+        return (this.answer_pool.length > 2 && this.answer_pool.length < 6);
     };
     Question.prototype.shufflePool = function () {
         var size = this.answer_pool.length;
@@ -173,7 +202,7 @@ var Question = (function () {
             return;
         for (var i = 0; i < size; i++) {
             //find a random position in the array
-            var pos = Helper.getRandomPosInArray(i, size);
+            var pos = helper.getRandomPosInArray(i, size);
             //change with current position
             var temp = this.answer_pool[pos];
             this.answer_pool[pos] = this.answer_pool[i];
@@ -182,8 +211,10 @@ var Question = (function () {
     };
     return Question;
 }());
-var q1 = new Question("A switch is a router?", new Answer("No", 5), false);
-q1.addAnswerToPool(new Answer("Yes", 0));
+var q1 = new Question("Javascript code can run on?", new Answer("All most common browsers(firefox, IR, Chrome, Safari etc.)", 2), false);
+q1.addAnswerToPool(new Answer("Internet Explorer only", 0));
+q1.addAnswerToPool(new Answer("Firefox only", 0));
+q1.addAnswerToPool(new Answer("Chrome only", 0));
 var q2 = new Question("Select the operating system in the provided list?", new Answer("Linux", 4), true);
 q2.addAnswerToPool(new Answer("HTML", 0));
 q2.addAnswerToPool(new Answer("TypeScript", 0));
@@ -193,8 +224,36 @@ var q3 = new Question("How to add an external JavaScript to an html page?", new 
 q3.addAnswerToPool(new Answer("<body></body>", 0));
 q3.addAnswerToPool(new Answer("<link hfref=''>", 0));
 q3.addAnswerToPool(new Answer("<div></div>", 0));
-q3.addAnswerToPool(new Answer("<head></head>", 0));
+//q3.addAnswerToPool(new Answer("<head></head>", 0));
 q3.addAnswerToPool(new Answer("<javascript></javascript>", 0));
+var q4 = new Question("A switch is a?", new Answer("Layer 2 device", 2), false);
+q4.addAnswerToPool(new Answer("Layer 3 device", 0));
+q4.addAnswerToPool(new Answer("Layer 1 device", 0));
+var q5 = new Question("A router is a?", new Answer("Layer 3 device", 2), false);
+q5.addAnswerToPool(new Answer("Layer 2 device", 0));
+q5.addAnswerToPool(new Answer("Layer 1 device", 0));
 var quiz = new Quiz();
-quiz.addQuestionsToPool(q1, q2, q3);
-console.log(quiz.toString());
+quiz.addQuestionsToPool(q1, q2, q3, q4, q5);
+var divQuiz = document.getElementById("quiz");
+for (var _i = 0, _a = quiz.questions; _i < _a.length; _i++) {
+    var question = _a[_i];
+    var ans_list = document.createElement("ol");
+    ans_list.innerHTML = question.getSentence();
+    for (var _b = 0, _c = question.getAllAnswers(); _b < _c.length; _b++) {
+        var answer = _c[_b];
+        var ans = document.createElement("li");
+        var radio = document.createElement("input");
+        radio.setAttribute("type", "radio");
+        radio.setAttribute("name", question.id.toString());
+        radio.setAttribute("value", question.id.toString());
+        radio.setAttribute("id", answer.id.toString());
+        var label = document.createElement("label");
+        label.setAttribute("for", answer.id.toString());
+        label.innerHTML = answer.definition.toString();
+        ans.appendChild(radio);
+        ans.appendChild(label);
+        ans_list.appendChild(ans);
+    }
+    divQuiz.appendChild(ans_list);
+}
+//# sourceMappingURL=Quiz.js.map
--- src/QuizApp/Quiz.ts
+++ src/QuizApp/Quiz.ts
@@ -1,4 +1,5 @@
-﻿namespace Helper {
+﻿namespace helper {
+
     let idAnswer = 0, idQuestion = 0;
 
     export function getIdAnswer() {
@@ -23,9 +24,15 @@
         }
     }
 
+    export function htmlTagReplace(data)
+    {
+        return data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    }
 }
 
-class Quiz{
+
+class Quiz {
+
     questions: Question[];
 
     constructor() {
@@ -33,10 +40,9 @@ class Quiz{
     }
 
     addQuestionsToPool(...questions: Question[]) {
-        for (let question of questions)
-        {
+        for (let question of questions) {
             this.questions.push(question);
-        } 
+        }
     }
 
     isValid() {
@@ -50,7 +56,7 @@ class Quiz{
 
         for (let question of this.questions) {
 
-            res += question.toString() + "\n"; 
+            res += question.toString() + "\n";
         }
 
         return res;
@@ -61,12 +67,24 @@ class Quiz{
 
         let score = 0;
 
-        for (let question of this.questions)
-        {
+        for (let question of this.questions) {
             score += question.score;
         }
 
         return score;
+    }
+
+    //maximum score for the quiz
+    maxScore() {
+
+        let score = 0;
+
+        for (let question of this.questions) {
+            score += question.maxScore;
+        }
+
+        return score;
+
     }
 
 
@@ -84,136 +102,149 @@ class Quiz{
 }
 
 class Answer {
-    private _id: number;
+        private _id: number;
 
-    constructor(private _definition: string, private _score: number) {
-        this._id = Helper.getIdAnswer();
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    get definition() {
-        return this.id + ' - ' + this._definition;
-    }
-
-    get score() {
-        return this._score;
-    }
-
-    set score(value) {
-        if (value >= 0) {
-            this._score = value;
-        } else {
-            throw new Error("Score should be greater or equal to zero!");
-        }
-    }
-
-    decreaseScore() {
-
-        if (this._score > 0) {
-            this._score--;
+        constructor(private _definition: string, private _score: number) {
+            this._id = helper.getIdAnswer();
+            this._definition = helper.htmlTagReplace(this._definition);
         }
 
+        get id() {
+            return this._id;
+        }
+
+        get definition() {
+            return this._definition;
+        }
+
+        get score() {
+            return this._score;
+        }
+
+        set score(value) {
+            if (value >= 0) {
+                this._score = value;
+            } else {
+                throw new Error("Score should be greater or equal to zero!");
+            }
+        }
+
+        decreaseScore() {
+
+            if (this._score > 0) {
+                this._score--;
+            }
+
+        }
     }
-}
 
 class Question {
-    private _id: number;
-    private answer_pool:Answer[];
-    private final_score: number;
+        private _id: number;
+        private answer_pool: Answer[];
+        private final_score: number;
 
-    constructor(private sentence: string, private answer: Answer, private canRetake: boolean) {
-        this.answer_pool = [];
-        this.answer_pool.push(answer);
-        this.final_score = -1;
-        this._id = Helper.getIdQuestion();
-    }
+        constructor(private sentence: string, private answer: Answer, private canRetake: boolean) {
 
-    get id() {
-        return this._id;
-    }
-
-    get score() {
-        return this.final_score == -1 ? 0 : this.final_score;
-    }
-
-    toString() {
-
-        let res = this.id + " - " + this.sentence + "\n";
-
-        let i = 0;
-
-        for (let answer of this.answer_pool)
-        {
-            res += /*(++i) + " - " +*/ answer.definition + "\n";
+            this.sentence = helper.htmlTagReplace(this.sentence);
+            this.answer_pool = [];
+            this.answer_pool.push(answer);
+            this.final_score = -1;
+            this._id = helper.getIdQuestion();
         }
 
-        return res;
-    }
-
-    addAnswerToPool(answer: Answer) {
-        this.answer_pool.push(answer);
-        this.shufflePool();
-    }
-
-    isCorrectAnswer(answer_id: number) {
-
-        //if >0 => question already answered correctly.
-        if (this.final_score > 0)
-            throw new Error("Question already answered!");
-
-        //cannot retake question
-        if (this.final_score === 0 && !this.canRetake)
-            throw new Error("You cannot retake this question!");
-
-
-        if (answer_id === this.answer.id) {
-            this.final_score = this.answer.score;
-            return true;
-        } else if (this.answer_pool.every(x => x.id !== answer_id)) {
-            throw new Error("Selected answer not in the list!");
-        }
-        else {
-            this.final_score = 0;
-            this.answer.decreaseScore();
-            return false;
-        }
-    }
-
-    isValidForQuiz() {
-        //at least 2 answers in order to be eligible for quiz => question is valid
-        return (this.answer_pool.length > 1);
-    }
-
-    private shufflePool() {
-
-        let size = this.answer_pool.length;
-
-        if (size <= 1) return;
-
-        for (let i = 0; i < size; i++) {
-
-            //find a random position in the array
-            var pos = Helper.getRandomPosInArray(i, size);
-
-            //change with current position
-            var temp = this.answer_pool[pos];
-            this.answer_pool[pos] = this.answer_pool[i];
-            this.answer_pool[i] = temp;
+        getAllAnswers() {
+            return this.answer_pool;
         }
 
+        getSentence() {
+            return this.sentence;
+        }
+
+        get id() {
+            return this._id;
+        }
+
+        get score() {
+            return this.final_score == -1 ? 0 : this.final_score;
+        }
+
+        get maxScore() {
+            return this.answer.score;
+        }
+
+        toString() {
+
+            let res = this.id + " - " + this.sentence + "\n";
+
+            let i = 0;
+
+            for (let answer of this.answer_pool) {
+                res += /*(++i) + " - " +*/ answer.definition + "\n";
+            }
+
+            return res;
+        }
+
+        addAnswerToPool(answer: Answer) {
+            this.answer_pool.push(answer);
+            this.shufflePool();
+        }
+
+        isCorrectAnswer(answer_id: number) {
+
+            //if >0 => question already answered correctly.
+            if (this.final_score > 0)
+                throw new Error("Question already answered!");
+
+            //cannot retake question
+            if (this.final_score === 0 && !this.canRetake)
+                throw new Error("You cannot retake this question!");
+
+
+            if (answer_id === this.answer.id) {
+                this.final_score = this.answer.score;
+                return true;
+            } else if (this.answer_pool.every(x => x.id !== answer_id)) {
+                throw new Error("Selected answer not in the list!");
+            }
+            else {
+                this.final_score = 0;
+                this.answer.decreaseScore();
+                return false;
+            }
+        }
+
+        isValidForQuiz() {
+            //1 of the requirements - Give the user a minimum of 3 and no more than 5 choices
+            //between 3 and 5 answers in order to be valid question for the quiz
+            return (this.answer_pool.length > 2 && this.answer_pool.length < 6);
+        }
+
+        private shufflePool() {
+
+            let size = this.answer_pool.length;
+
+            if (size <= 1) return;
+
+            for (let i = 0; i < size; i++) {
+
+                //find a random position in the array
+                var pos = helper.getRandomPosInArray(i, size);
+
+                //change with current position
+                var temp = this.answer_pool[pos];
+                this.answer_pool[pos] = this.answer_pool[i];
+                this.answer_pool[i] = temp;
+            }
+
+        }
     }
-}
 
 
-
-
-
-
-let q1 = new Question("A switch is a router?", new Answer("No", 5), false);
-q1.addAnswerToPool(new Answer("Yes", 0));
+let q1 = new Question("Javascript code can run on?", new Answer("All most common browsers(firefox, IR, Chrome, Safari etc.)", 2), false);
+q1.addAnswerToPool(new Answer("Internet Explorer only", 0));
+q1.addAnswerToPool(new Answer("Firefox only", 0));
+q1.addAnswerToPool(new Answer("Chrome only", 0));
 
 
 let q2 = new Question("Select the operating system in the provided list?", new Answer("Linux", 4), true);
@@ -227,15 +258,48 @@ let q3 = new Question("How to add an external JavaScript to an html page?", new 
 q3.addAnswerToPool(new Answer("<body></body>", 0));
 q3.addAnswerToPool(new Answer("<link hfref=''>", 0));
 q3.addAnswerToPool(new Answer("<div></div>", 0));
-q3.addAnswerToPool(new Answer("<head></head>", 0));
+//q3.addAnswerToPool(new Answer("<head></head>", 0));
 q3.addAnswerToPool(new Answer("<javascript></javascript>", 0));
 
+let q4 = new Question("A switch is a?", new Answer("Layer 2 device", 2), false);
+q4.addAnswerToPool(new Answer("Layer 3 device", 0));
+q4.addAnswerToPool(new Answer("Layer 1 device", 0));
+
+let q5 = new Question("A router is a?", new Answer("Layer 3 device", 2), false);
+q5.addAnswerToPool(new Answer("Layer 2 device", 0));
+q5.addAnswerToPool(new Answer("Layer 1 device", 0));
 
 let quiz = new Quiz();
-quiz.addQuestionsToPool(q1, q2, q3);
+quiz.addQuestionsToPool(q1, q2, q3, q4, q5);
 
-console.log(quiz.toString());
+let divQuiz = document.getElementById("quiz");
 
+for (let question of quiz.questions) {
 
+    let ans_list = document.createElement("ol");
 
+    ans_list.innerHTML = question.getSentence();
 
+    for (let answer of question.getAllAnswers()) {
+
+        let ans = document.createElement("li");
+
+        let radio = document.createElement("input");
+        radio.setAttribute("type", "radio");
+        radio.setAttribute("name", question.id.toString());
+        radio.setAttribute("value", question.id.toString());
+        radio.setAttribute("id", answer.id.toString());
+
+        let label = document.createElement("label");
+        label.setAttribute("for", answer.id.toString());
+        label.innerHTML = answer.definition.toString();
+
+        ans.appendChild(radio);
+        ans.appendChild(label);
+
+        ans_list.appendChild(ans);
+    }
+
+    divQuiz.appendChild(ans_list);
+
+}
